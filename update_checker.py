c544da11063b09508fce1cac6ba8b24cafc7b535
Fabio <Fabio@delphimeasurement.com>
2026-01-08 13:07:20 +1000

    Release v0.0.12

--- update_checker.py
+++ update_checker.py
@@ -11,7 +11,7 @@ import webbrowser
 from typing import Optional, Callable, Tuple
 
 # Current version - UPDATE THIS ON EACH RELEASE
-VERSION = "0.0.11"
+VERSION = "0.0.12"
 
 # GitHub repository info
 GITHUB_REPO = "ClarkeFL/project-launch"
